d0096deb4d0d6963008017e07090180764e0407e
Ajeet Singh <9008308+jeetgeg@users.noreply.github.com>
2025-01-01 09:52:50 +0530

    home page text changed

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,12 @@
 export default function Index() {
-    return (
+  return (
+    <div>
+      <h1>Welcome to the App</h1>
       <p id="index-page">
-        This is a demo for Remix.
+        This is a just for fun.
         <br />
-        Check out{" "}
-        <a href="https://remix.run">the docs at remix.run</a>.
+        Check out <a href="https://remix.run">the docs at remix.run</a>.
       </p>
-    );
-  }
+    </div>
+  );
+}
